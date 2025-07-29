43c3d7501659f0a509d91840fcfdb396a576ef67
Zinaida Prishutova <belyaevazina94@gmail.com>
2025-07-29 18:08:00 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+ public class BmiService {
+        public int calculate(int weight, double height) {
+            int result = (int) (weight / (height * height));
+
+            return result;
+        }
+    }
